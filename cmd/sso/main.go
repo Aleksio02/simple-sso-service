6bc339df28882f8f67935acfcc4efe0ba227c347
Aleksio02 <oleks1o@yandex.ru>
2025-01-04 18:42:01 +0300

    [master] Return to REST

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"html/template"
 	"simple-sso-service/modules/sso/routes"
 )
 
@@ -11,17 +10,5 @@ func main() {
 
 	routes.RegisterSsoRoutes(app)
 
-	app.Static("/assets", "./modules/sso/assets")
-
-	//tmpl := template.Must(template.ParseGlob("modules/sso/templates/*.html"))
-	tmpl := template.Must(template.ParseFiles(
-		"modules/sso/templates/base.html",
-		"modules/sso/templates/register.html",
-		"modules/sso/templates/login.html",
-	))
-	app.SetHTMLTemplate(tmpl)
-
-	//app.LoadHTMLGlob("modules/sso/templates/*.html")
-	//app.LoadHTMLGlob("modules/sso/templates/layout/*")
 	app.Run(":8080")
 }
--- modules/sso/controller/sso_controller.go
+++ modules/sso/controller/sso_controller.go
@@ -1,33 +1,9 @@
 package controller
 
 import (
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
-func RenderLoginPage(c *gin.Context) {
-	session := sessions.Default(c)
-	//var data struct {
-	//	RedirectLink string `json:"redirect-link"`
-	//}
-
-	//c.ShouldBindJSON(&data)
-	//fmt.Println(data)
-	//session.Set("redirect-link", data.RedirectLink)
-
-	session.Set("redirect-link", "http://localhost:8081/somePageToRedirect")
-	session.Save()
-	c.HTML(http.StatusOK, "login.html", nil)
-}
-
-func RenderRegisterPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "register.html", nil)
-}
-
 func HandleLoginRequest(c *gin.Context) {
-	session := sessions.Default(c)
-	redirectLink := session.Get("redirect-link").(string)
-	session.Delete("redirect-link")
-	c.Redirect(http.StatusFound, redirectLink)
+	// TODO: implement me
 }
